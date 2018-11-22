3b754e04fafadbb4556e382efd185212ee5f8d69
dev637 <devang713@gmail.com>
2018-11-22 10:04:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Barbershop
+
+Barbershop is a mock-up of a men's hair salon website and it was built using Gatsby.
+
+## Built With
+
+- [Gatsby](https://github.com/gatsbyjs/gatsby) - Development environment
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }} />
+  </Layout>
+)
+
+export default IndexPage
