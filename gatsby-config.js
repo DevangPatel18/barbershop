3d44405df9161799f9e89cc066fd61b4aac6a496
dev637 <devang713@gmail.com>
2018-11-25 23:30:09 -0500

    Add SectionHeader styling
    Add styled components library with Gatsby plugin to add component styling.

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,15 @@ module.exports = {
         icon: 'src/images/barber-pole.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Neuton', 'Roboto:300']
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
+
+const SectionHeaderStyles = styled.div`
+  h2 {
+    font-family: 'Neuton';
+    font-size: 72px;
+    font-weight: 100;
+    color: #c2a300;
+
+    :after {
+      content: '';
+      display: block;
+      margin-top: 20px;
+      width: 104px;
+      border-top: 3px solid #9f9f9f;
+    }
+  }
+  p {
+    font-family: 'Roboto';
+    font-size: 24px;
+    font-weight: 100;
+    color: #757575;
+    padding-top: 20px;
+  }
+`
 
 const SectionHeader = props => {
   const { headerTitle, content } = props
 
   return (
-    <>
+    <SectionHeaderStyles>
       <h2>{headerTitle}</h2>
       <p>{content}</p>
-    </>
+    </SectionHeaderStyles>
   )
 }
 
