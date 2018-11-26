e268ee1d067153fbac4db61d7d27688b8aec2064
dev637 <devang713@gmail.com>
2018-11-26 15:57:21 -0500

    Add home section
    Added home section, including background, logo, and link to reservation section.

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,64 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import styled from 'styled-components'
+import logo from '../images/Logo.svg'
+
+const HeaderStyles = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: url('https://res.cloudinary.com/dbeqp2lyo/image/upload/v1543258768/Barbershop/barber-2345701_1920.jpg');
+    background-size: cover;
+    background-position: center;
+    filter: saturate(0) brightness(0.22);
+    z-index: -1;
+  }
+  
+  .header-logo {
+    height: 67%;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+  }
+
+  .header-reserve {
+    height: 33%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  img {
+    width: 20vw;
+    min-width: 350px;
+    margin: 0;
+  }
+  
+  a {
+    color: white;
+    background: none;
+    padding: .85rem;
+    border: 5px solid #c2a300;
+    font-family: 'Roboto';
+    font-weight: 600;
+    font-size: 24px;
+    text-decoration: none;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderStyles>
+    <div className="header-logo"><img src={logo} alt="" /></div>
+    <div className="header-reserve"><a href='#reservations'>BOOK RESERVATION</a></div>
+  </HeaderStyles>
 )
 
 export default Header
