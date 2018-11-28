e3aea98c42c7db408908cfe594a7aed3e53ea726
dev637 <devang713@gmail.com>
2018-11-28 15:11:32 -0500

    Add date and time picker to Reservations section
    Added flatpickr library to add data and time picker to Reservations section.

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SectionHeader from './SectionHeader'
 import styled from 'styled-components'
 import Button from './Button'
+import Flatpickr from 'react-flatpickr'
+import 'flatpickr/dist/themes/airbnb.css'
+const MINUTE_INCREMENT = 20
 
 const ReservationsStyles = styled.section`
   position: relative;
-  height: 60vh;
+  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -23,22 +26,68 @@ const ReservationsStyles = styled.section`
     background-position: center;
     z-index: -1;
   }
+
+  .reservations-form {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding-top: 5em;
+
+    input {
+      font-family: Roboto;
+      font-size: 24px;
+      padding: 20px;
+      margin-right: 2em;
+      width: 18em;
+      text-align: center;
+      font-weight: bolder;
+      border: 1px solid black;
+    }
+  }
 `
 
-const Reservations = () => {
-  return (
-    <ReservationsStyles id="reservations">
-      <SectionHeader
-        white
-        center
-        headerTitle="Reservations"
-        content="Proident adipisicing duis id dolor non occaecat ea pariatur mollit deserunt consectetur proident minim incididunt."
-      />
-      <div>
-        <Button href="#" text="RESERVE" />
-      </div>
-    </ReservationsStyles>
-  )
+const currentTime = () => {
+  let coeff = 1000 * 60 * MINUTE_INCREMENT
+  let date = new Date()
+  let rounded = new Date(Math.round(date.getTime() / coeff) * coeff)
+  return rounded
+}
+
+class Reservations extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      date: currentTime(),
+    }
+  }
+  render() {
+    const { date } = this.state
+    return (
+      <ReservationsStyles id="reservations">
+        <SectionHeader
+          white
+          center
+          headerTitle="Reservations"
+          content="Proident adipisicing duis id dolor non occaecat ea pariatur mollit deserunt consectetur proident minim incididunt."
+        />
+        <div className="reservations-form">
+          <Flatpickr
+            data-enable-time
+            options={{
+              dateFormat: 'F j, Y - h:i K',
+              minuteIncrement: MINUTE_INCREMENT,
+            }}
+            value={date}
+            onChange={date => {
+              this.setState({ date })
+            }}
+          />
+          <Button href="#" text="RESERVE" />
+        </div>
+      </ReservationsStyles>
+    )
+  }
 }
 
 export default Reservations
