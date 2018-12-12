7709b2c4b1b0b305df530e454459b810562090d0
dev637 <devang713@gmail.com>
2018-12-11 23:05:24 -0500

    Add styled components for About section

--- src/components/About.js
+++ src/components/About.js
@@ -6,43 +6,76 @@ const AboutStyles = styled.div`
   display: flex;
   flex-direction: row;
 
-  .about-bg {
-    position: relative;
-    width: 50%;
-    height: auto;
+  @media (max-width: 760px) {
+    height: 100%;
+    flex-direction: column;
+  }
+
+  @media (min-width: 761px) {
+    min-height: 100vh;
+  }
+`
+
+const AboutBackgroundStyles = styled.div`
+  position: relative;
+  width: 50%;
+  height: auto;
+  background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 1) 180%
+    ),
+    linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 1) 95%
+    ),
+    url('https://res.cloudinary.com/dbeqp2lyo/image/upload/v1543266272/Barbershop/barber-1017457_1920.jpg');
+  background-size: cover;
+  background-position: center;
+  z-index: -1;
+
+  ::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-right: 1px solid white;
+  }
+
+  @media (max-width: 760px) {
+    width: 100%;
+    min-height: 300px;
+
     background: linear-gradient(
-        to right,
-        rgba(255, 255, 255, 0),
-        rgba(255, 255, 255, 1) 180%
+        to bottom,
+        rgba(255, 255, 255, 0) 60%,
+        rgba(255, 255, 255, 1) 150%
       ),
       linear-gradient(
-        to right,
-        rgba(255, 255, 255, 0),
+        to bottom,
+        rgba(255, 255, 255, 0) 60%,
         rgba(255, 255, 255, 1) 95%
       ),
       url('https://res.cloudinary.com/dbeqp2lyo/image/upload/v1543266272/Barbershop/barber-1017457_1920.jpg');
     background-size: cover;
     background-position: center;
-    z-index: -1;
 
     ::before {
-      content: '';
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      border-right: 1px solid white;
+      border-right: none;
+      border-bottom: 1px solid white;
     }
   }
+`
 
-  .about {
-    width: 50%;
-    background: white;
-    margin: 0 auto;
-    padding: 6rem 0;
-    display: flex;
-    align-items: center;
-    transition: all 0.5s;
-  }
+const AboutContainerStyles = styled.div`
+  width: 50%;
+  background: white;
+  margin: 0 auto;
+  padding: 6rem 0;
+  display: flex;
+  align-items: center;
+  transition: all 0.5s;
 
   .about-content {
     max-width: 600px;
@@ -51,55 +84,22 @@ const AboutStyles = styled.div`
   }
 
   @media (max-width: 760px) {
+    width: 100%;
     height: 100%;
-    flex-direction: column;
-
-    .about-bg {
-      width: 100%;
-      min-height: 300px;
-
-      background: linear-gradient(
-          to bottom,
-          rgba(255, 255, 255, 0) 60%,
-          rgba(255, 255, 255, 1) 150%
-        ),
-        linear-gradient(
-          to bottom,
-          rgba(255, 255, 255, 0) 60%,
-          rgba(255, 255, 255, 1) 95%
-        ),
-        url('https://res.cloudinary.com/dbeqp2lyo/image/upload/v1543266272/Barbershop/barber-1017457_1920.jpg');
-      background-size: cover;
-      background-position: center;
-
-      ::before {
-        border-right: none;
-        border-bottom: 1px solid white;
-      }
-    }
-
-    .about {
-      width: 100%;
-      height: 100%;
-      bottom: 0;
-      padding: 0;
-      align-items: flex-start;
-    }
+    bottom: 0;
+    padding: 0;
+    align-items: flex-start;
 
     .about-content {
       margin: 0;
     }
   }
-
-  @media (min-width: 761px) {
-    min-height: 100vh;
-  }
 `
 
 const About = () => (
   <AboutStyles id="about">
-    <div className="about-bg" />
-    <div className="about">
+    <AboutBackgroundStyles />
+    <AboutContainerStyles>
       <div className="about-content">
         <SectionHeader
           headerTitle={'Our Story'}
@@ -108,7 +108,7 @@ const About = () => (
           }
         />
       </div>
-    </div>
+    </AboutContainerStyles>
   </AboutStyles>
 )
 
