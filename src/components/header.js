fa3116319af4c72ad74a8c19dd55d9e5c6f35317
dev637 <devang713@gmail.com>
2018-12-11 19:46:59 -0500

    Add styled components for header section
    Added styled components for header section elements. Also added dark background to improve contrast with logo on page load.

--- src/components/header.js
+++ src/components/header.js
@@ -9,6 +9,8 @@ const HeaderStyles = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
+  background: #282828;
+  z-index: 0;
 
   ::before {
     content: '';
@@ -23,93 +25,89 @@ const HeaderStyles = styled.div`
     filter: saturate(0) brightness(0.22);
     z-index: -1;
   }
+`
 
-  .header-logo {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-transform: uppercase;
-    padding: 4rem 0;
-    font-size: 20px;
+const HeaderLogoStyles = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  padding: 4rem 0;
+  font-size: 20px;
 
+  img {
+    width: 300px;
+    margin: 0;
+  }
+
+  p {
+    font-size: 1em;
+    margin: 0.5em;
+  }
+
+  .header-logo-top {
+    font-family: Roboto;
+    color: #c2a300;
+  }
+
+  .header-logo-bottom {
+    color: white;
+  }
+
+  @media (min-width: 761px) {
     img {
-      width: 300px;
-      margin: 0;
+      width: calc(300px + ((1vw - 7.61px) * 27.1));
     }
 
-    p {
-      font-size: 1em;
-      margin: 0.5em;
+    font-size: calc(20px + ((1vw - 7.61px) * 2.71));
+  }
+
+  @media (min-width: 1130px) {
+    img {
+      width: calc(400px + ((1vw - 11.3px) * 15));
     }
 
-    &-top {
-      font-family: Roboto;
-      color: #c2a300;
-    }
+    font-size: 30px;
+  }
+`
 
-    &-bottom {
-      color: white;
+const HeaderInfoStyles = styled.div`
+  color: #b9b9b9;
+  text-align: center;
+  text-transform: none;
+  margin: 1rem 0 2rem;
+  p {
+    @media (max-width: 760px) {
+      font-size: 12px;
     }
 
     @media (min-width: 761px) {
-      img {
-        width: calc(300px + ((1vw - 7.61px) * 27.1));
-      }
-
-      font-size: calc(20px + ((1vw - 7.61px) * 2.71));
+      font-size: calc(12px + ((1vw - 7.61px) * 2.439));
     }
 
     @media (min-width: 1130px) {
-      img {
-        width: calc(400px + ((1vw - 11.3px) * 15));
-      }
-
-      font-size: 30px;
+      font-size: 21px;
     }
   }
 
-  .header-reserve {
-    margin-top: 2rem;
-  }
+  .divider {
+    color: #c2a300;
 
-  .header-info {
-    color: #b9b9b9;
-    text-align: center;
-    text-transform: none;
-    margin-top: 1rem;
-    p {
-      @media (max-width: 760px) {
-        font-size: 12px;
-      }
-
-      @media (min-width: 761px) {
-        font-size: calc(12px + ((1vw - 7.61px) * 2.439));
-      }
-
-      @media (min-width: 1130px) {
-        font-size: 21px;
-      }
-    }
-
-    .divider {
-      color: #c2a300;
-
-      ::before {
-        content: '|';
-      }
+    ::before {
+      content: '|';
     }
   }
 `
 
 const Header = ({ siteTitle }) => (
   <HeaderStyles id="home">
-    <div className="header-logo">
+    <HeaderLogoStyles>
       <p className="header-logo-top">traditional men's hair salon</p>
       <img src={logo} alt="logo" />
       <p className="header-logo-bottom">classic & modern cuts</p>
-      <div className="header-info">
+      <HeaderInfoStyles>
         <p>
           M - F: 9 am - 7 pm <span className="divider" /> SAT: 10 am - 5 pm{' '}
           <span className="divider" /> SUN: Closed
@@ -117,11 +115,9 @@ const Header = ({ siteTitle }) => (
         <p>
           (416) 999 9991 <span className="divider" /> barbs@shop.com
         </p>
-      </div>
-      <div className="header-reserve">
-        <Button href="#reservations" text="BOOK RESERVATION" />
-      </div>
-    </div>
+      </HeaderInfoStyles>
+      <Button href="#reservations" text="BOOK RESERVATION" />
+    </HeaderLogoStyles>
   </HeaderStyles>
 )
 
