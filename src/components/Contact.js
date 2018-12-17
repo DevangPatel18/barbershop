69233854caadbdc9d7e667df2e3183b74ce49dab
dev637 <devang713@gmail.com>
2018-12-17 10:47:04 -0500

    Add labels and change styling to Contact form
    Added labels for input fields to improve accessibility, and changed section styling.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,13 +9,13 @@ const ContactStyles = styled.section`
   position: relative;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
   padding: 4rem 0;
   background: white;
   z-index: 0;
 
   @media (max-width: 760px) {
-    padding: 4rem 0;
+    padding: 2rem 0;
   }
 `
 
@@ -83,16 +83,25 @@ const ContactBackgroundStyles = styled(Img)`
 const ContactFormStyles = styled.form`
   text-align: center;
   font-family: 'Roboto';
-  margin: 0 auto;
+  margin: 2rem 0;
   max-width: 600px;
-  padding: 0 1rem;
+  padding: 1rem;
   z-index: 1;
+  background: rgba(100, 100, 100, 0.4);
+
+  label {
+    display: block;
+    text-align: left;
+    margin-bottom: 1rem;
+    text-transform: capitalize;
+    color: white;
+  }
 
   .contact-form-top {
     display: flex;
     justify-content: space-between;
-    input[id='name'],
-    input[id='phone'] {
+
+    label {
       width: 48%;
     }
   }
@@ -102,12 +111,7 @@ const ContactFormStyles = styled.form`
     opacity: 0.8;
     width: 100%;
     padding: 0.3rem 0.5rem;
-    margin-top: 1.2rem;
-    border: 1px solid black;
-  }
-
-  ::placeholder {
-    color: #9f9f9f;
+    margin-top: 5px;
   }
 
   textarea {
@@ -116,10 +120,11 @@ const ContactFormStyles = styled.form`
   }
 
   @media (max-width: 760px) {
+    margin: 0;
+    width: 90%;
     .contact-form-top {
       flex-direction: column;
-      input[id='name'],
-      input[id='phone'] {
+      label {
         width: 100%;
       }
     }
@@ -157,44 +162,45 @@ class Contact extends Component {
 
             <ContactFormStyles>
               <div className="contact-form-top">
-                <input
-                  type="text"
-                  placeholder="Name"
-                  id="name"
-                  name="name"
-                  autoComplete="nope"
-                  required
-                />
-                <input
-                  type="text"
-                  placeholder="Phone"
-                  id="phone"
-                  name="phone"
-                  autoComplete="nope"
-                  required
-                />
+                <label htmlFor="name">
+                  name
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    autoComplete="nope"
+                    required
+                  />
+                </label>
+                <label htmlFor="phone">
+                  phone
+                  <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    autoComplete="nope"
+                    required
+                  />
+                </label>
               </div>
-              <input
-                type="email"
-                placeholder="Email"
-                id="email"
-                name="email"
-                autoComplete="nope"
-                required
-              />
-              <input
-                type="text"
-                placeholder="Subject"
-                id="subject"
-                name="subject"
-              />
-              <textarea
-                rows="6"
-                placeholder="Message"
-                id="message"
-                name="message"
-                required
-              />
+              <label htmlFor="email">
+                email
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  autoComplete="nope"
+                  required
+                />
+              </label>
+              <label htmlFor="subject">
+                subject
+                <input type="text" id="subject" name="subject" />
+              </label>
+              <label htmlFor="message">
+                message
+                <textarea rows="6" id="message" name="message" required />
+              </label>
               <Button submit />
             </ContactFormStyles>
           </ContactStyles>
