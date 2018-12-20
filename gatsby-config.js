9c70880f92c3135e249cdcd7caa0be0e7d4423ab
dev637 <devang713@gmail.com>
2018-12-20 10:04:36 -0500

    Add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-svgr',
+    'gatsby-plugin-offline',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
